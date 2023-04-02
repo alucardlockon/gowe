1799f12197fbc0bbbf402594d1c6b0ec80c77962
alucardlockon <alucardlockon3@gmail.com>
2023-04-02 21:51:21 +0800

    fix: use kotlin

--- gowe-server/gowe-module-system/gowe-module-system-start/src/main/kotlin/com/gowe/GoweSystemApplication.kt
+++ gowe-server/gowe-module-system/gowe-module-system-start/src/main/kotlin/com/gowe/GoweSystemApplication.kt
@@ -1,16 +1,23 @@
 package com.gowe
 
-import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
 
 /**
  * GOWE后台启动类
  */
 @SpringBootApplication
-@Slf4j
 open class GoweSystemApplication
+
+
+private val log = LoggerFactory.getLogger(GoweSystemApplication::class.java)
 
 fun main(args: Array<String>) {
     runApplication<GoweSystemApplication>(*args)
+    log.info("gowe started !!")
 }
+
+
+
